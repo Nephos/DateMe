221adc70398f32bdd7079f993e59b2cce53e14e7
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-01-04 03:24:10 +0100

    better rights management
    - can read anything
    - can manage everything owned by the user
    - can create a new meeting_date if user match with the meeting admin
    - can create a new user_date if the user match
    - admin can do everything he want
    
    Still, there is no user interface yet, so the crud is visible and
    accessible to simpe users. When the final user
    interface (MeetingMakerController) will be finished, the CRUD will be
    reserved to the admin.
    
    Ability is still a garantee
    
    attach and partialy fix #1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,22 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
+    # You can read if you have the url
     can :read, Meeting
     can :read, MeetingDate
     can :read, UserDate
+
+    # You can manage if you created this
     can :manage, Meeting, :user_id => user.id
+    can :manage, MeetingDate, :meeting => {:user_id => user.id}
+    can :manage, UserDate, :meeting_date => { :meeting => {:user_id => user.id}}
+
+    # Add new data to an existing meeting
+    can :new, MeetingDate
+    can :create, MeetingDate, :meeting => {:user_id => user.id}, :user_id => user.id
+    can :create, UserDate, :user_id => user.id
 
     can :manage, :all if user.admin?
-    #can :manage, :all, user.is_admin => true
-    #cannot :manage, :all, user.is_admin => false
 
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
