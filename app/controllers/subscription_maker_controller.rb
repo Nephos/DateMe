1185cc409b9d9a6d0e1d5b858a505ce73702ec64
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-04-03 21:54:30 +0200

    fix sql error when subscribing to empty poll
    - fix sql error
    - message when empty poll (so cannot subscribe)
    - improve code
    
    close #11

--- app/controllers/subscription_maker_controller.rb
+++ app/controllers/subscription_maker_controller.rb
@@ -4,15 +4,13 @@ class SubscriptionMakerController < PrivateController
   def create
     @meeting = Meeting.find(params[:meeting_id])
     meeting_date_ids = @meeting.meeting_dates.pluck(:id)
+    return redirect_to(meeting_share_path(@meeting), alert: "You cannot subscribe to an empty poll") if meeting_date_ids.size.zero?
     data = meeting_date_ids.map{|meeting_date_id| {user_id: current_user.id, meeting_date_id: meeting_date_id} }
     # The following code comes from the 90' !!
-    n = ActiveRecord::Base.transaction do
-      t = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-      sql_query_mass_insert = "INSERT INTO user_dates (\"user_id\", \"meeting_date_id\", \"updated_at\", \"created_at\") VALUES " + data.map {|row|
-        "(#{row[:user_id]}, #{row[:meeting_date_id]}, '#{t}', '#{t}')"
-      }.join(", ")
-      UserDate.connection.execute sql_query_mass_insert
-    end.cmd_tuples
+    t = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    sql_query_mass_insert = "INSERT INTO user_dates (\"user_id\", \"meeting_date_id\", \"updated_at\", \"created_at\") VALUES "
+    sql_query_mass_insert += data.map {|row| "(#{row[:user_id]}, #{row[:meeting_date_id]}, '#{t}', '#{t}')" }.join(", ")
+    n = ActiveRecord::Base.transaction { UserDate.connection.execute sql_query_mass_insert }.cmd_tuples
     redirect_to({action: :show, controller: :meeting_maker}, {notice: "Added to the poll, and #{n} vote#{n>1??s:nil} prepared for you"})
   end
 
