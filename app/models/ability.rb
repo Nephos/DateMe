e0408bd539407b2017116c2658a8ca0a2c8f9abb
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-04-03 11:06:58 +0200

    remove all CRUD access to non-admin users
    close #1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,22 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    # You can read if you have the url
-    can :read, Meeting
-    can :read, MeetingDate
-    can :read, UserDate
-
-    # You can manage if you created this
-    can :manage, Meeting, :user_id => user.id
-    can :manage, MeetingDate, :meeting => {:user_id => user.id}
-    can :manage, UserDate, :meeting_date => { :meeting => {:user_id => user.id}}
-
-    # Add new data to an existing meeting
-    can :new, MeetingDate
-    can :manage, MeetingDate, :meeting => {:user_id => user.id}
-    can :manage, UserDate, :user_id => user.id
-
-    can :manage, :all if user.admin?
+    if user.admin?
+      can :manage, :all
+    else
+      cannot :manage, Meeting
+      cannot :manage, MeetingDate
+      cannot :manage, UserDate
+      cannot :manage, User
+    end
 
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
